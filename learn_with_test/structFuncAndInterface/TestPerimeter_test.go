f66eeb36fff346d63e14b8860bb51dea173f1a3b
bamg.zhou <bamg.zhou@cardinfolink.com>
2021-07-06 14:37:16 +0800

    feature(结构体方法和接口): 新增结构体目录
    - 新增 test 文件: ../learn_with_test/structFuncAndInterface/TestPerimeter.go
    add struct 结构体
    - 修改 test 文件:
     add testArea

--- learn_with_test/structFuncAndInterface/TestPerimeter_test.go
+++ learn_with_test/structFuncAndInterface/TestPerimeter_test.go
@@ -4,8 +4,18 @@ import (
 	"testing"
 )
 
+func TestArea(t *testing.T) {
+	rectangle := Rectangle{12.0, 6.0}
+	got := Area(rectangle)
+	want := 72.0
+	if got != want {
+		t.Errorf("got %.2f want %.2f ", got, want)
+	}
+}
+
 func TestPerimeter(t *testing.T) {
-	got := Perimeter(10.0, 10.0)
+	rectangle := Rectangle{10.0, 10.0}
+	got := Perimeter(rectangle)
 	want := 40.0
 	if got != want {
 		t.Errorf("got %0.2f want %.2f", got, want)
