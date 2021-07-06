9ca3634d1847a4370031be03aa1398bf6b5f32ca
bamg.zhou <bamg.zhou@cardinfolink.com>
2021-07-06 14:15:49 +0800

    feature(结构体方法和接口): 新增结构体目录
    - 新增 controller 文件: xxx/xxx
    - 新增 model 文件： xxx/xxx
    - 新增 service 文件： xxx/xxx

--- learn_with_test/structFuncAndInterface/TestPerimeter_test.go
+++ learn_with_test/structFuncAndInterface/TestPerimeter_test.go
@@ -1,1 +1,13 @@
 package structFuncAndInterface
+
+import (
+	"testing"
+)
+
+func TestPerimeter(t *testing.T) {
+	got := Perimeter(10.0, 10.0)
+	want := 40.0
+	if got != want {
+		t.Errorf("got %0.2f want %.2f", got, want)
+	}
+}
