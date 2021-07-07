f6585089e1b16d04b91080e32467c5d0bcbafe56
bamg.zhou <bamg.zhou@cardinfolink.com>
2021-07-07 22:47:15 +0800

    feature(结构体方法和接口): 修改文件
    - 修改 TestPerimeter.go 文件:
      - 修改集中 test方法
    - 修改 TestPerimeter.go 文件: ../TestPerimeter.go
        - 增加 shape interface
        - 增加 Rectangle 和 Circle 的Area()方法

--- learn_with_test/structFuncAndInterface/TestPerimeter_test.go
+++ learn_with_test/structFuncAndInterface/TestPerimeter_test.go
@@ -5,22 +5,12 @@ import (
 )
 
 func TestArea(t *testing.T) {
-	checkArea := func(t *testing.T, shape Shape, want float64) {
-		t.Helper()
-		got := shape.Area()
-		if got != want {
-			t.Errorf("got %.2f want %.2f", got, want)
-		}
+	areaTests := []struct {
+		shape Shape
+		want  float64
+	}{
+		{Rectangle{}},
 	}
-	t.Run("rectangles", func(t *testing.T) {
-		rectangle := Rectangle{12, 6}
-		checkArea(t, rectangle, 72.0)
-	})
-
-	t.Run("circles", func(t *testing.T) {
-		circle := Circle{10}
-		checkArea(t, circle, 314.1592653589793)
-	})
 }
 
 func TestPerimeter(t *testing.T) {
