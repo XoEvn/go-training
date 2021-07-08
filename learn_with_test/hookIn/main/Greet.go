099fe651917141307756e2af0bf856a0d7ce2e73
bamg.zhou <bamg.zhou@cardinfolink.com>
2021-07-08 17:29:57 +0800

    feature(add file): 增加测试文件

--- learn_with_test/hookIn/main/Greet.go
+++ learn_with_test/hookIn/main/Greet.go
@@ -3,13 +3,16 @@ package main
 import (
 	"fmt"
 	"io"
-	"os"
+	"net/http"
 )
 
-func main() {
-	Greet(os.Stdout, "Elodie")
+func Greet(writer io.Writer, name string) {
+	fmt.Fprintf(writer, "Hello, %s", name)
+}
+func MyGreeterHandler(w http.ResponseWriter, r *http.Request) {
+	Greet(w, "world")
 }
 
-func Greet(writer io.Writer, name string) {
-	fmt.Fprintf(writer, "Hello,%s", name)
+func main() {
+	http.ListenAndServe(":5000", http.HandlerFunc(MyGreeterHandler))
 }
