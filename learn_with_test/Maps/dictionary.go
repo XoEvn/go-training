be75dbffe10f7041b09651933d27aaf5eaa09644
bamg.zhou <bamg.zhou@cardinfolink.com>
2021-07-08 16:10:34 +0800

    feature(add file): 增加测试文件

--- learn_with_test/Maps/dictionary.go
+++ learn_with_test/Maps/dictionary.go
@@ -1,15 +1,33 @@
 package Maps
 
-import (
-	"errors"
-)
-
 type Dictionary map[string]string
 
-var ErrNotFound = errors.New("could not find the word you were looking for")
+const (
+	ErrNotFound   = DictionaryErr("could not find the word you were looking for")
+	ErrWordExists = DictionaryErr("cannot add word because it already exists")
+)
 
-func (d Dictionary) Add(word, definition string) {
+type DictionaryErr string
+
+func (e DictionaryErr) Error() string {
+	return string(e)
+}
+
+func (d Dictionary) Update(word, definition string) {
 	d[word] = definition
+}
+
+func (d Dictionary) Add(word, definition string) error {
+	_, err := d.Search(word)
+	switch err {
+	case ErrNotFound:
+		d[word] = definition
+	case nil:
+		return ErrWordExists
+	default:
+		return err
+	}
+	return nil
 }
 
 func (d Dictionary) Search(word string) (string, error) {
--- learn_with_test/Maps/dictionary_test.go
+++ learn_with_test/Maps/dictionary_test.go
@@ -6,18 +6,54 @@ import (
 
 var thisIsAJustTest = "this is a just test"
 
-var m map[string]string
+// 因为map是引用类型,所以如果直接初始化会得到一个nil异常
+// 使用 make 或者 在尾部加上{}可以
+//var m map[string]string
+//
+//
+//
+//func TestMapNil(t *testing.T){
+//	var dictionary = make(map[string]string)
+//	if dictionary == nil {
+//		t.Errorf("maps is nil not init")
+//	}
+//}
+
+func TestUpdate(t *testing.T) {
+	word := "test"
+	definition := "this is just a test"
+	dictionary := Dictionary{word: definition}
+	newDefinition := "new definition"
+	dictionary.Update(word, newDefinition)
+	assertDefinition(t, dictionary, word, newDefinition)
+}
 
 func TestAdd(t *testing.T) {
-	dictionary := Dictionary{}
-	dictionary.Add("test", "this is just a test")
-	want := "this is just a test"
-	got, err := dictionary.Search("test")
+	t.Run("new word", func(t *testing.T) {
+		dictionary := Dictionary{}
+		word := "test"
+		definition := "this is just a test"
+		dictionary.Add(word, definition)
+		assertDefinition(t, dictionary, word, definition)
+	})
+	t.Run("existing word", func(t *testing.T) {
+		word := "test"
+		definition := "this is just test"
+		dictionary := Dictionary{word: definition}
+		err := dictionary.Add(word, "new test")
+		assertError(t, err, ErrWordExists)
+		assertDefinition(t, dictionary, word, definition)
+	})
+}
+
+func assertDefinition(t *testing.T, dictionary Dictionary, word, definition string) {
+	t.Helper()
+	got, err := dictionary.Search(word)
 	if err != nil {
 		t.Fatal("should find added word:", err)
 	}
-	if want != got {
-		t.Errorf("got '%s' want '%s'", got, want)
+	if definition != got {
+		t.Errorf("got '%s' want '%s'", got, definition)
 	}
 }
 
