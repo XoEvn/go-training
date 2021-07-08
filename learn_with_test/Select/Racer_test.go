16b04a6cffeb12e18fa004f84228bf88dde078c5
bamg.zhou <bamg.zhou@cardinfolink.com>
2021-07-08 17:51:08 +0800

    feature(select): 进程同步
    -将流水式的同步修改成进程同步

--- learn_with_test/Select/Racer_test.go
+++ learn_with_test/Select/Racer_test.go
@@ -1,15 +1,32 @@
 package Select
 
 import (
+	"net/http"
+	"net/http/httptest"
 	"testing"
+	"time"
 )
 
 func TestRacer(t *testing.T) {
-	slowURL := "http://www.facebook.com"
-	fastURL := "http://www.quii.co.ul"
+	slowServer := makeDelayedServer(20 * time.Microsecond)
+	fastServer := makeDelayedServer(0 * time.Microsecond)
+
+	defer slowServer.Close()
+	defer fastServer.Close()
+	slowURL := slowServer.URL
+	fastURL := fastServer.URL
+
 	want := fastURL
 	got := Racer(slowURL, fastURL)
 	if got != want {
 		t.Errorf("got '%s' want '%s'", got, want)
 	}
+
+}
+
+func makeDelayedServer(delay time.Duration) *httptest.Server {
+	return httptest.NewServer(http.HandlerFunc(func(writer http.ResponseWriter, request *http.Request) {
+		time.Sleep(delay)
+		writer.WriteHeader(http.StatusOK)
+	}))
 }
